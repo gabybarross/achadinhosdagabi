a150d2e297aeae112b480f942ad22758d613bbad
Your Name <you@example.com>
2026-02-06 09:00:14 -0300

    atualizacao banco

---
+++ publicar_ofertas.bat
@@ -0,0 +1,11 @@
+@echo off
+echo ==========================================
+echo      CONVERTENDO EXCEL PARA O SITE
+echo ==========================================
+echo.
+python converter_excel_para_site.py
+echo.
+echo ==========================================
+echo           PROCESSO FINALIZADO
+echo ==========================================
+pause
