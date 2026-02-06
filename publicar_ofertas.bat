f827dddc50a807c324864ee1fcedaae8facb4885
Your Name <you@example.com>
2026-02-06 09:15:29 -0300

    atualiza

--- publicar_ofertas.bat
+++ publicar_ofertas.bat
@@ -6,6 +6,14 @@ echo.
 python converter_excel_para_site.py
 echo.
 echo ==========================================
+echo      PUBLICANDO NO GITHUB (SITE)
+echo ==========================================
+echo.
+git add ofertas.js
+git commit -m "Atualizacao via Excel (Manual)"
+git push origin main
+echo.
+echo ==========================================
 echo           PROCESSO FINALIZADO
 echo ==========================================
 pause
